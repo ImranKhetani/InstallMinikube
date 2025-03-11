75002f15d8ccc29968d2e7aa26a3696a327b51af
Imran Khetani <89381192+ImranKhetani@users.noreply.github.com>
2025-03-11 08:37:10 +0530

    Create install_minikube.sh

---
+++ install_minikube.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y docker.io conntrack
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --driver=docker
